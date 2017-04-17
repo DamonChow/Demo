80aa54031c23774038154782869da278135aef8b
zhoujiwei <damon_chow@163.com>
2017-04-17 09:07:59 +0800

    string split 功能对比

--- src/main/java/com/damon/test/StringSplitTest.java
+++ src/main/java/com/damon/test/StringSplitTest.java
@@ -1,6 +1,6 @@
 package com.damon.test;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
