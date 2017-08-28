4ef137969f452a86440d61de6ab88bc92302c50e
Damon <damon_chow@163.com>
2017-08-28 09:24:28 +0800

    mybatis generator   maven插件执行。

---
+++ mybatis/mybatis-generator/src/main/java/com/damon/plugin/LombokPlugin.java
@@ -0,0 +1,201 @@
+package com.damon.plugin;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * A MyBatis Generator plugin to use Lombok's annotations.
+ * For example, use @Data annotation instead of getter ands setter.
+ *
+ * @author Paolo Predonzani (http://softwareloop.com/)
+ */
+public class LombokPlugin extends PluginAdapter {
+
+    private final Collection<Annotations> annotations;
+
+    /**
+     * LombokPlugin constructor
+     */
+    public LombokPlugin() {
+        annotations = new LinkedHashSet<Annotations>(Annotations.values().length);
+    }
+
+    /**
+     * @param warnings
+     * @return always true
+     */
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    /**
+     * Intercepts base record class generation
+     *
+     * @param topLevelClass
+     * @param introspectedTable
+     * @return
+     */
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass,
+                                                 IntrospectedTable introspectedTable) {
+        addDataAnnotation(topLevelClass);
+        return true;
+    }
+
+    /**
+     * Intercepts primary key class generation
+     *
+     * @param topLevelClass
+     * @param introspectedTable
+     * @return
+     */
+    @Override
+    public boolean modelPrimaryKeyClassGenerated(TopLevelClass topLevelClass,
+                                                 IntrospectedTable introspectedTable) {
+        addDataAnnotation(topLevelClass);
+        return true;
+    }
+
+    /**
+     * Intercepts "record with blob" class generation
+     *
+     * @param topLevelClass
+     * @param introspectedTable
+     * @return
+     */
+    @Override
+    public boolean modelRecordWithBLOBsClassGenerated(
+            TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        addDataAnnotation(topLevelClass);
+        return true;
+    }
+
+    /**
+     * Prevents all getters from being generated.
+     * See SimpleModelGenerator
+     *
+     * @param method
+     * @param topLevelClass
+     * @param introspectedColumn
+     * @param introspectedTable
+     * @param modelClassType
+     * @return
+     */
+    @Override
+    public boolean modelGetterMethodGenerated(Method method,
+                                              TopLevelClass topLevelClass,
+                                              IntrospectedColumn introspectedColumn,
+                                              IntrospectedTable introspectedTable,
+                                              ModelClassType modelClassType) {
+        return false;
+    }
+
+    /**
+     * Prevents all setters from being generated
+     * See SimpleModelGenerator
+     *
+     * @param method
+     * @param topLevelClass
+     * @param introspectedColumn
+     * @param introspectedTable
+     * @param modelClassType
+     * @return
+     */
+    @Override
+    public boolean modelSetterMethodGenerated(Method method,
+                                              TopLevelClass topLevelClass,
+                                              IntrospectedColumn introspectedColumn,
+                                              IntrospectedTable introspectedTable,
+                                              ModelClassType modelClassType) {
+        return false;
+    }
+
+    /**
+     * Adds the lombok annotations' imports and annotations to the class
+     *
+     * @param topLevelClass
+     */
+    private void addDataAnnotation(TopLevelClass topLevelClass) {
+        for (Annotations annotation : annotations) {
+            topLevelClass.addImportedType(annotation.javaType);
+            topLevelClass.addAnnotation(annotation.name);
+        }
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+        super.setProperties(properties);
+
+        //@Data is default annotation
+        annotations.add(Annotations.DATA);
+
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            boolean isEnable = Boolean.parseBoolean(entry.getValue().toString());
+
+            if (isEnable) {
+                String paramName = entry.getKey().toString().trim();
+                Annotations annotation = Annotations.getValueOf(paramName);
+                if (annotation != null) {
+                    annotations.add(annotation);
+                    annotations.addAll(Annotations.getDependencies(annotation));
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean clientGenerated(Interface interfaze,
+                                   TopLevelClass topLevelClass,
+                                   IntrospectedTable introspectedTable) {
+        interfaze.addImportedType(new FullyQualifiedJavaType("org.apache.ibatis.annotations.Mapper"));
+        interfaze.addAnnotation("@Mapper");
+        return true;
+    }
+
+    private enum Annotations {
+        DATA("data", "@Data", "lombok.Data"),
+        BUILDER("builder", "@Builder", "lombok.Builder"),
+        ALL_ARGS_CONSTRUCTOR("allArgsConstructor", "@AllArgsConstructor", "lombok.AllArgsConstructor"),
+        NO_ARGS_CONSTRUCTOR("noArgsConstructor", "@NoArgsConstructor", "lombok.NoArgsConstructor"),
+        TO_STRING("toString", "@ToString", "lombok.ToString");
+
+
+        private final String paramName;
+        private final String name;
+        private final FullyQualifiedJavaType javaType;
+
+
+        Annotations(String paramName, String name, String className) {
+            this.paramName = paramName;
+            this.name = name;
+            this.javaType = new FullyQualifiedJavaType(className);
+        }
+
+        private static Annotations getValueOf(String paramName) {
+            for (Annotations annotation : Annotations.values())
+                if (String.CASE_INSENSITIVE_ORDER.compare(paramName, annotation.paramName) == 0)
+                    return annotation;
+
+            return null;
+        }
+
+        private static Collection<Annotations> getDependencies(Annotations annotation) {
+            if (annotation == ALL_ARGS_CONSTRUCTOR)
+                return Collections.singleton(NO_ARGS_CONSTRUCTOR);
+            else
+                return Collections.emptyList();
+        }
+    }
+}
